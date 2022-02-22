203b838744366133bc1272e3d0652595974e75bd
emsthf <deunsol916@gmail.com>
2022-02-22 06:06:07 +0000

    TodoItem click event test ok

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-function TodoItem({ todo }) {
+function TodoItem({ todo, onToggle, onRemove }) {
   const { id, text, done } = todo; // prop으로 들어론 todo를 개별로 분리
 
   return (
     <li>
-      <span>{text}</span>
-      <button>삭제</button>
+      <span
+        style={{ textDecration: done ? "line-through" : "none" }}
+        onClick={() => onToggle(id)}
+      >
+        {text}
+      </span>
+      <button onClick={() => onRemove(id)}>삭제</button>
     </li>
   );
 }
--- src/TodoItem.test.js
+++ src/TodoItem.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import React from "react";
 import TodoItem from "./TodoItem";
 
@@ -22,5 +22,26 @@ describe("<TodoItem />", () => {
     const { span, button } = setup();
     expect(span).toBeTruthy();
     expect(button).toBeTruthy();
+  });
+
+  it("does not show line-through on span when done is false", () => {
+    const { span } = setup({ todo: { ...sampleTodo, done: false } });
+    expect(span).not.toHaveStyle("text-decoration: line-through;"); // 해당 스타일을 가지고 있지 않는지
+  });
+
+  it("calls onToggle", () => {
+    const onToggle = jest.fn();
+    const { span } = setup({ onToggle });
+
+    fireEvent.click(span);
+
+    expect(onToggle).toBeCalledWith(sampleTodo.id); // 토글이 되면 id 값이 호출이 되는지
+  });
+
+  it("calls onRemove", () => {
+    const onRemove = jest.fn();
+    const { button } = setup({ onRemove });
+    fireEvent.click(button);
+    expect(onRemove).toBeCalledWith(sampleTodo.id); // onRemove가 되면 id값이 호출이 되는지
   });
 });
