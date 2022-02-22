b76e29d796dc5b16cdfee4d6d6331567ea3c6afb
emsthf <deunsol916@gmail.com>
2022-02-22 02:47:19 +0000

    TodoForm Submit event test ok

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,13 +1,21 @@
 import React, { useState, useCallback } from "react";
 
-function TodoForm() {
+function TodoForm({ onInsert }) {
   const [value, setValue] = useState("");
   const onChange = useCallback((e) => {
     setValue(e.target.value);
   }, []);
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value); // submit이 되면 value를 가져와서 onInsert에 넣어줌
+      setValue(""); // input 초기화
+      e.preventDefault(); // 새로고침 방지
+    },
+    [onInsert, value]
+  );
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <input placeholder="할 일을 입력하세요~" value={value} onChange={onChange} />
       <button type="submit">등록</button>
     </form>
--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -17,4 +17,17 @@ describe("<TodoForm />", () => {
     expect(input).toHaveAttribute("value", "TDD 배우기"); // value라는 속성이 'TDD 배우기'를 가지고 있는지
     // 변화가 감지되려면 useState를 사용해야 한다
   });
+
+  it("call onInsert and clears input", () => {
+    const onInsert = jest.fn(); // mock 함수
+    const { getByText, getByPlaceholderText } = render(<TodoForm onInsert={onInsert} />);
+    const input = getByPlaceholderText("할 일을 입력하세요~");
+    const button = getByText("등록");
+
+    fireEvent.change(input, { target: { value: "TDD 배우기" } });
+    fireEvent.click(button);
+
+    expect(onInsert).toBeCalledWith("TDD 배우기");
+    expect(input).toHaveAttribute("value", ""); // 버튼 클릭 후 인풋이 초기화 되는지
+  });
 });
