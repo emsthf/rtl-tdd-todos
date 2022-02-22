f2e744d43a10d39d8f96076a29557e06907c0459
emsthf <deunsol916@gmail.com>
2022-02-22 03:19:38 +0000

    TodoForm refactoring ok

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -3,15 +3,22 @@ import React from "react";
 import TodoForm from "./TodoForm";
 
 describe("<TodoForm />", () => {
+  const setup = (props = {}) => {
+    const utils = render(<TodoForm {...props} />); // TodoForm의 모든 props를 넘겨준다
+    const { getByText, getByPlaceholderText } = utils;
+    const input = getByPlaceholderText("할 일을 입력하세요~");
+    const button = getByText("등록");
+    return { ...utils, input, button };
+  };
+
   it("has input and a button", () => {
-    const { getByText, getByPlaceholderText } = render(<TodoForm />);
-    getByPlaceholderText("할 일을 입력하세요~"); // input이 있는지 없는지
-    getByText("등록"); // button이 있는지 없는지
+    const { input, button } = setup();
+    expect(input).toBeTruthy(); // 있는지 없는지 검사
+    expect(button).toBeTruthy();
   });
 
   it("change input", () => {
-    const { getByPlaceholderText } = render(<TodoForm />);
-    const input = getByPlaceholderText("할 일을 입력하세요~");
+    const { input } = setup();
 
     fireEvent.change(input, { target: { value: "TDD 배우기" } }); // json 타입으로 변경할 값을 적어주면 된다
     expect(input).toHaveAttribute("value", "TDD 배우기"); // value라는 속성이 'TDD 배우기'를 가지고 있는지
@@ -20,9 +27,7 @@ describe("<TodoForm />", () => {
 
   it("call onInsert and clears input", () => {
     const onInsert = jest.fn(); // mock 함수
-    const { getByText, getByPlaceholderText } = render(<TodoForm onInsert={onInsert} />);
-    const input = getByPlaceholderText("할 일을 입력하세요~");
-    const button = getByText("등록");
+    const { input, button } = setup({ onInsert }); // props가 있으니 setup에 prop을 넘겨준다
 
     fireEvent.change(input, { target: { value: "TDD 배우기" } });
     fireEvent.click(button);
